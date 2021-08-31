e8f61793e7334b2e28fbdab3330c1a68972666e0
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-08-31 15:18:05 +0530

    How to find the kth largest elements in the array

--- Sorting_the_array.java
+++ Sorting_the_array.java
@@ -1,6 +1,6 @@
 package data_structure;
 class Reverse{
-    public  int[] sort(int arr[]){
+    public  int sortkthlargest(int arr[] ,int position){
      for(int i=0;i<arr.length;i++)
      {
          for(int j=i+1;j<arr.length;j++)
@@ -14,18 +14,14 @@ class Reverse{
              }
          }
      }
-     return arr;
+     return arr[(arr.length-1)-position];
     }
 }
 public class Sorting_the_array{
-
     public static void main(String[] args) {
         int arr[]={1,5,2,7,8,9,2,3,6,5};
         Reverse re=new Reverse();
-        re.sort(arr);
-        for(int ele:arr)
-        {
-            System.out.print(ele+" "+"\t");
-        }
+        int pos=re.sortkthlargest(arr,3);
+        System.out.println(pos);
     }
 }
