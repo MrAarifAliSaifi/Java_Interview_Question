825b6f2dde66d7e819ec59f6464c35e7791f5847
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-08-31 14:52:49 +0530

    How to reverse array in java

--- Reverse_Array.java
+++ Reverse_Array.java
@@ -1,25 +1,25 @@
-package aarif.com;
-public class Reverse_Array {
-    public int array( int arr[])
+public class Reverse_Array{
+    public int[] reverse(int arr1[])
     {
-        int len= arr.length;
-        int var=0;
-
-        for(int i=0;i<=len;i++)
+        int len=arr1.length;
+        int i=0,j=len-1;
+        for(;i<len;i++)
         {
-             var=var+i;
+            int temp=arr1[i];
+            arr1[i]=arr1[j];
+            arr1[j]=temp;
+            i++;
+            j--;
         }
-        return var;
+        return arr1;
     }
     public static void main(String[] args) {
         Reverse_Array obj=new Reverse_Array();
         int arr[]={1,2,3,4,5,6,7};
-        int var2=obj.array(arr);
-        System.out.println(var2);
+        obj.reverse(arr);
+        for(int ele:arr)
+        {
+            System.out.print(ele+"\t");
+        }
     }
 }
-/*
-char[]={'a','a','r','i,'f'};
-String str="aarif";
-*/
-
